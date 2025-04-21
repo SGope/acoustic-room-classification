ca69f842be9e32501c2ba660b3d7a78409950dcb
SGope <sayantangope.ger@gmail.com>
2025-04-21 14:19:24 +0200

    Update final_set.py

--- code/final_set.py
+++ code/final_set.py
@@ -5,19 +5,19 @@ import shutil
 
 def main():
     # Split set into test, train folders
-    # path = "/home/issac/PycharmProjects/room_classification/dataset_for_students/BRIRs/DH/"
-    # train_path = "/home/issac/PycharmProjects/room_classification/dataset_for_students/Final/Train/DH/"
-    # test_path = "/home/issac/PycharmProjects/room_classification/dataset_for_students/Final/Test/DH/"
-    # filenames = [file for file in os.listdir(path) if file.endswith(".wav")]
-    # print(len(filenames))
-    # #random.shuffle(filenames)
-    # train_set, test_set = train_test_split(filenames, test_size=0.3, train_size=0.7, shuffle=True)
-    # for files in sorted(train_set):
-    #     shutil.copy(os.path.join(path, files), os.path.join(train_path, files))
-    # for files in sorted(test_set):
-    #     shutil.copy(os.path.join(path, files), os.path.join(test_path, files))
-    # print(len(train_set))
-    # print(len(test_set))
+    path = "/___ Enter path ___/BRIRs/DH/"
+    train_path = "/___ Enter path ___/Final/Train/DH/"
+    test_path = "/___ Enter path ___/Final/Test/DH/"
+    filenames = [file for file in os.listdir(path) if file.endswith(".wav")]
+    print(len(filenames))
+    #random.shuffle(filenames)
+    train_set, test_set = train_test_split(filenames, test_size=0.3, train_size=0.7, shuffle=True)
+    for files in sorted(train_set):
+        shutil.copy(os.path.join(path, files), os.path.join(train_path, files))
+    for files in sorted(test_set):
+        shutil.copy(os.path.join(path, files), os.path.join(test_path, files))
+    print(len(train_set))
+    print(len(test_set))
 
     precision = 10.987
     recall = 7.9876
