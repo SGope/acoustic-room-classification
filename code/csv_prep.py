0aa448abeed93a065907306c34a4702b0e7ddd8a
SGope <sayantangope.ger@gmail.com>
2025-04-21 14:14:36 +0200

    Update csv_prep.py

--- code/csv_prep.py
+++ code/csv_prep.py
@@ -4,12 +4,12 @@ import os
 class Prep():
     def convert(self, file):
         print(file)
-        path = "___ Enter path ___"
+        path = "/___ Enter path ___/"
         df = pd.read_csv(os.path.join(path, file))
 
 
     def get_files(self):
-        path = "___ Enter path ___"
+        path = "/___ Enter path ___/"
         filenames = [file for file in os.listdir(path) if file.endswith(".csv")]
 
         for file in sorted(filenames):
