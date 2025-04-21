2721140e2e4e21d93eabf708195984d597f6703b
SGope <sayantangope.ger@gmail.com>
2025-04-21 13:53:42 +0200

    Update csv_prep.py

--- code/csv_prep.py
+++ code/csv_prep.py
@@ -3,14 +3,13 @@ import os
 
 class Prep():
     def convert(self, file):
-        """"""
         print(file)
-        path = "/home/issac/PycharmProjects/room_classification/dataset_for_students/Parameter_csv/Parameter_csv/"
+        path = "___ Enter path ___"
         df = pd.read_csv(os.path.join(path, file))
 
 
     def get_files(self):
-        path = "/home/issac/PycharmProjects/room_classification/dataset_for_students/Parameter_csv/Parameter_csv/"
+        path = "___ Enter path ___"
         filenames = [file for file in os.listdir(path) if file.endswith(".csv")]
 
         for file in sorted(filenames):
